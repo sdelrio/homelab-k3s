3e2f02ba9effbc6737c5a92bb695597e47fa1112
sdelrio <oftheriver@gmail.com>
2022-11-27 13:45:12 +0100

    bootstrap: init argocd

---
+++ bootstrap/root/apply.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+VALUES="values.yaml"
+
+helm template \
+    --include-crds \
+    --namespace argocd \
+    --values "${VALUES}" \
+    argocd . \
+    | kubectl apply -n argocd -f -
+
